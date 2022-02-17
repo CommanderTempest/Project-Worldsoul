a6b463bfbc757451e3ce77c64bcd15e2834aef47
CommanderTempest <krukovtempest@gmail.com>
2022-02-17 11:28:52 -0600

    Changed Roact setup
    I turned my Roact UI into a functional based setup. I am now also using bindings to update the position value of my hotbar buttons.

--- src/client/handleUI.client.tsx
+++ src/client/handleUI.client.tsx
@@ -1,36 +1,67 @@
-import Roact from "@rbxts/roact";
-import {Players} from "@rbxts/services";
+import Roact, { update } from "@rbxts/roact";
+import {Players, UserInputService} from "@rbxts/services";
 
+// the player's UI
 let playerGui = Players.LocalPlayer.FindFirstChild("PlayerGui") as PlayerGui;
 
-let myHotbarRefs = new Array<Roact.Ref>();
+function hotbarUI()
+{
+  const [uiPosition, updateButtonPosition] = Roact.createBinding(new UDim2(0,0,0,0))
+  return (
+    <screengui ResetOnSpawn={false}>
+      <frame 
+        
+        Size={new UDim2(0, 900, 0, 50)} 
+        Position={new UDim2(0, 200, 0, 474)} // Center bottom
+        BackgroundColor3={new Color3(138, 138, 138)} // A lightish gray
+        >
 
-let slot1 = Roact.createRef<TextButton>();
+        <textbutton 
+          Event = {{MouseButton1Down: (x, y) => updateButtonPosition(moveButton(x,y)),
+            // Where I left off:
+            /* How do I constantly update the UI's position, yet check if the button is still down? Could use a coroutine?*/
+          MouseButton1Up: () => updateButtonPosition(releaseButton())}}
+        
+          Size = {new UDim2(0, 112.5, 0, 50)}
+          Position = {uiPosition}
+          
+        />
 
-let hotbarUI = (
-  <screengui ResetOnSpawn={false}>
-    <frame>
-      <textbutton 
-        Event= {{MouseButton1Down: () => moveButton}}
-        Ref = {slot1}
-      />
-    </frame>
-  </screengui>
-)
+        <imagelabel
+          Size = {new UDim2(0, 44, 0, 50)}
+          Position = {new UDim2(0, 896, 0, 0)}
+          // Insert image into here too
+        />
+      </frame>
+    </screengui>
+  ) // end of return
+}
 
-slot1.getValue(); // I can then access textbutton1's properties
-
-let myHandle = Roact.mount(<screengui></screengui>, playerGui, "hotbarUI");
+let myHandle = Roact.mount(hotbarUI(), playerGui, "hotbarUI");
 
 //*********************************************************
 
 // the moveButton function moves the button UI from the hotbar 
 // and follows the player's mouse until released
 
-function moveButton(x: number, y: number) 
+function moveButton(x: TextButton, y: number) : UDim2
 {
-  //Players.LocalPlayer.GetMouse().
   
+  let myNewPosition;
+
+  // this while loop constantly checks if the left mouse is being pressed
+  // then will update the position of the action UI if it is.
+
+  if (UserInputService.IsMouseButtonPressed("MouseButton1"))
+  {
+    print("RUNNING!");
+    wait(0.01);
+    // if this works right, follow the player's mouse and update myAction's position
+    return new UDim2(0, Players.LocalPlayer.GetMouse().X, 0, Players.LocalPlayer.GetMouse().Y);
+  } // end while
+
+  print("DETECT ELSE?");
+  return new UDim2(0, 0, 0, 0);
 } 
 
 //*********************************************************
@@ -38,7 +69,8 @@ function moveButton(x: number, y: number)
 // releaseButton handles either dropping a hotbar action
 // onto a blank spot, or swapping if otherwise
 
-function releaseButton()
+function releaseButton() : UDim2
 {
-
-}
+  print("Mouse released");
+  return new UDim2(0,0,0,0);
+} // end releaseButton
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,3 +1,11 @@
-import { makeHello } from "shared/module";
+// Project Worldsoul
 
-print(makeHello("main.server.ts"));
+// Basic RPG style combat, like any traditional version.
+// This is just for practice understandable...
+
+/*
+  The client will handle hotbar actions, and will simply send the ability up to be calculated and processed.
+  When the ability is sent up, the server will check if the ability matches what the player actually has, 
+  and then use it depending on resource pools.
+*/
+export {}
